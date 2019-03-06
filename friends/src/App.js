94bb86d083bbc70601c76b23a98b9d94a5f1be92
conconrob <connortprobertson@gmail.com>
2019-03-06 13:38:01 +0000

    DONE setting up and trimming

---
+++ friends/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  width:500px;
+  margin: 50px auto;
+`
+class App extends Component {
+  render() {
+    return (
+     <StyledApp>
+       hi
+     </StyledApp>
+    );
+  }
+}
+
+export default App;
